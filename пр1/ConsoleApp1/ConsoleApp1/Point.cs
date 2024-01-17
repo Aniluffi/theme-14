ebc1045581136a88547e041660588d30e04314d7
Aniluffi <90216018+Aniluffi@users.noreply.github.com>
2024-01-17 22:08:26 +0300

    Add files via upload

---
+++ пр1/ConsoleApp1/ConsoleApp1/Point.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Point
+    {
+        int y;
+        int x;
+        static int count;
+        int count2 = Point.count + 1;//добавил новое приватное поле чтобы при показом длины между двумя точками мы могли увидеть номера точек у которых мы ищем длину между друг другом 
+        
+
+        public int Y { get => y; set => y = value; }
+        public int X { get => x; set => x = value; }
+   
+
+        public Point ()
+        {
+            PointNumber();
+            Print();
+        }
+
+        public Point(int x,int y)
+        {
+            PointNumber();
+
+            this.X = x;
+            this.Y = y;
+
+            Print();
+        }
+
+        static Point()
+        {
+           
+        }
+
+        private void PointNumber()
+        {
+           
+            Point.count += 1;
+         
+        }
+
+        private void Print()
+        {
+            Console.WriteLine($"Экземпляр №{count}\n" +
+                $"Координаты Y = {Y}\n" +
+                $"Координаты X = {X}\n");
+
+        }
+
+        public static void LengthPoints(Point a,Point b)
+        {
+            Console.WriteLine($"длина между точкой №Х{a.count2} И №Х{b.count2} = {Math.Round(Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y + b.Y, 2)),2)}");
+        }
+    }
+}
---
+++ пр1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {  
+            Point len = new Point(1,2);
+            Point len2 = new Point();
+            Point len3 = new Point();
+            Point len4 = new Point(5,6);
+            Point len5 = new Point(10,2);
+            Point len6 = new Point();
+
+            Point.LengthPoints(len4,len5);
+            //len1.Print();
+            //len2.Print();
+            //len3.Print();
+            //len4.Print();
+            //len5.Print();
+            //len.Print();
+          
+        }
+    }
+}
